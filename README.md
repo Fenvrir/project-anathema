2add1cecc92ecbbb336af0c4f36f0be21f97e877
Fenvrir <53856227+Fenvrir@users.noreply.github.com>
2024-06-12 20:53:30 +0700

    Feature/webpack ts config (#2)
    * Added webpack and ts-config to project
    
    * added dist folder to gitignore
    
    * removed dist folder
    
    * Decomposed webpack-config

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 ## Anathema Project
 
-##Webpack
+## Webpack
 npm i -y = инифиализировать объект с дефолтными настроками.
-1) На вход Webpack-у мы передаём кучу файлов, а он уже генерирует для нас статику. Всего лишь несколько файлов.
-2) Webpack + webpack cli
-3) html-webpack-plugin 
-4) webpack progress plugin - прогресс бар в cli
-5) typescript  + ts-loader
-6) webpack config ts file - позволяет писать конфиг вебпака на ts
-7) настройка ts-config 
+1.    
+    1) На вход Webpack-у мы передаём кучу файлов, а он уже генерирует для нас статику. Всего лишь несколько файлов.
+    2) Webpack + webpack cli
+    3) html-webpack-plugin 
+    4) webpack progress plugin - прогресс бар в cli
+    5) typescript  + ts-loader
+    6) webpack config ts file - позволяет писать конфиг вебпака на ts
+    7) настройка ts-config
+
+2. 
+    1) В корне создаём папку config, там хранится вся конфикурация приложения. Webpack, eslint и тд.
+    2) создаём отдельные файлы в папке build для плагинов, лоадеров и резолверсов.
+    3) создаём папку types, а там создаём config.ts и внутри прописываем пути, и описываем типы сборок ( продакшин или девелопмент)
+    4) всё остальное из webpack.config  переносим  в buildwebpack.config
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,34 +1,23 @@
-import path from "path";
 import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import { BuildWebpackConfig } from "./config/build/buildWbpackConfig";
+import { BuildMode, BuildPaths } from "./config/build/types/config";
+import path from "path";
 
-const config: webpack.Configuration = {
-  mode: "development",
+const paths: BuildPaths = {
   entry: path.resolve(__dirname, "src", "index.ts"),
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    new webpack.ProgressPlugin(),
-  ],
+  build: path.resolve(__dirname, "dist"),
+  html: path.resolve(__dirname, "public", "index.html"),
 };
+
+const mode: BuildMode = "development";
+const isDev = mode === "development" ?? true;
+
+
+const config: webpack.Configuration = BuildWebpackConfig({
+  mode: "development",
+  paths,
+  isDev,
+});
+
 
 export default config;
