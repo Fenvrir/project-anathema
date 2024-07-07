5de25dc8f96e80b934ba8f937085398ceb670d8d
Fenvrir <dragonofhokage@gmail.com>
2024-07-07 23:53:19 +0300

    added hot module replacement

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,12 +3,13 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import type { WebpackPluginInstance } from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { BuildOptions } from "./types/config";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export const buildPlugins = ({
   paths,
   isDev,
 }: BuildOptions): WebpackPluginInstance[] => {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -16,9 +17,21 @@ export const buildPlugins = ({
     new webpack.DefinePlugin({
       __IS_DEV__: isDev,
     }),
+
     new MiniCssExtractPlugin({
       filename: "css/[name].[contenthash:8].css",
       chunkFilename: "css/[name].[contenthash:8].css",
     }),
   ];
+
+  // Если сборка для девелопа, тогда у нас должна быть горячая замена кода.
+  // Следовательно используем плагины для этого.
+  if (isDev) {
+    plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new ReactRefreshWebpackPlugin()
+    );
+  }
+
+  return plugins;
 };
